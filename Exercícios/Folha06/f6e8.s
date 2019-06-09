b18f27415a001305d56c02e62af394cec8215d43
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-06-09 22:17:21 +0100

    Update f6e8.s

--- Exercícios/Folha06/f6e8.s
+++ Exercícios/Folha06/f6e8.s
@@ -8,10 +8,10 @@ ciclo:
 	cbz			X2, fim
 	ldr			Q0, [X0], #16
 	ldr			Q1, [X1]
-	fabs			V1.4S, V1.4S //fabs(X[i])
-	fmul			V2.4S, V1.4S, V0.S[0] //da * fabs(X[i])
-	fsub			V1.4S, V0.4S, V2.4S //Y[i] - da * fabs(X[i])
-	str 			Q1, [X1], #16
+	fabs			V3.4S, V1.4S //fabs(X[i])
+	fmul			V2.4S, V3.4S, V0.S[0] //da * fabs(X[i])
+	fsub			V3.4S, V1.4S, V2.4S //Y[i] - da * fabs(X[i])
+	str 			Q3, [X1], #16
 	sub			X2, X2, #1
 	b			ciclo
 fim:
